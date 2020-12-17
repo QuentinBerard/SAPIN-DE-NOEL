7aca2625a9cab18001d84f70e2b2c8177c3e0ede
Quentin Berard <72550698+QuentinBerard@users.noreply.github.com>
2020-12-17 17:44:26 +0100

    Add files via upload

---
+++ sapin.py
@@ -0,0 +1,14 @@
+etoilesapin= '+'
+branchesapin='*'
+taillesapin= input ('Rentre un nombre tel que 1>nombre>7:')
+nombresapin= int (taillesapin)
+int(i)=1
+function branchedesapin(){
+print ('/',(branchesapin),'|')
+}
+while (i<nombresapin){
+i=i++
+print ('etoilesapin')
+branchedesapin()
+
+}
